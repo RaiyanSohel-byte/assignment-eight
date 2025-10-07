9b1846aa25531c37d47c0bfbfe50dcf3f35e144d
RaiyanSohel-byte <afnan19262@gmail.com>
2025-10-07 19:56:12 +0600

    footer added

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import { Outlet } from "react-router";
+import Footer from "./Footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
